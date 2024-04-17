3c01301013afc8eda0584a332f252e2de3fd49d3
dormon <imilet@fit.vutbr.cz>
2024-04-17 13:51:47 +0200

    Cool student filtering :)

--- src/main.cpp
+++ src/main.cpp
@@ -44,9 +44,35 @@ int main(int argc,char*argv[]){
 
   layout(binding=0)uniform sampler2D tex;
 
+  uniform int isFilterActivated = 1;
+
   out vec4 fColor;
   void main(){
-    fColor = texture(tex,vTexCoord);
+    vec3 color = texture(tex,vTexCoord).rgb;
+    if(isFilterActivated != 1){
+      fColor = vec4(color,1);
+      return;
+    }
+
+    // filter
+    vec2 A=vec2(0.5f,0.5f);
+    vec2 B=vec2(-1,1);
+    vec2 s = normalize(B-A);
+    vec2 n = vec2(-s.y,s.x);
+    vec3 p = vec3(n,-dot(n,A));
+
+    float dist = dot(p,vec3(vTexCoord,1.f));
+
+    float stripe = fract(dist*20);
+
+    if(color.r < 0.5)color *= vec3(stripe);
+
+
+    if(stripe < 3.f/3.f && stripe>=2.f/3.f){color.b = float((stripe-2/3.f)*3.f > 1-color.b);}
+    if(stripe < 2.f/3.f && stripe>=1.f/3.f){color.g = float((stripe-1/3.f)*3.f > 1-color.g);}
+    if(stripe < 1.f/3.f && stripe>=0.f/3.f){color.r = float((stripe-0/3.f)*3.f > 1-color.r);}
+
+    fColor = vec4(color,1);
 
   }
   ).");
@@ -69,20 +95,25 @@ int main(int argc,char*argv[]){
 
   bool running = true;
   float lastFrameTime = 0.f;
+
   while(running){// MAIN LOOP
     SDL_Event event;
     while(SDL_PollEvent(&event)){ // EVENT LOOP
       if(event.type == SDL_QUIT)running = false;
+      if(event.type == SDL_KEYDOWN){
+        if(event.key.keysym.sym == SDLK_f)prg->set1i("isFilterActivated",1);
+        if(event.key.keysym.sym == SDLK_g)prg->set1i("isFilterActivated",0);
+      }
     }
 
     glClearColor(.2,.2,1,1);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    float newTime = timer.elapsedFromStart();
-    if((newTime-lastFrameTime)>1./video.getFps()){
+    //float newTime = timer.elapsedFromStart();
+    //if((newTime-lastFrameTime)>1./video.getFps()){
       tex->setData2D(video.getData(),GL_BGR,GL_UNSIGNED_BYTE);
-      lastFrameTime = newTime;
-    }
+    //  lastFrameTime = newTime;
+    //}
 
 
     prg->use();
