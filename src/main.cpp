920cd167c6a484e6e6de6d7a700c041e44ab64fd
dormon <imilet@fit.vutbr.cz>
2024-04-17 12:29:28 +0200

    SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 
+#include<SDL.h>
+
 int main(int argc,char*argv[]){
   std::cerr << "Ahoj" << std::endl;
+  auto window = SDL_CreateWindow("MUL2024",0,0,1024,768,0);
+
+
+  SDL_DestroyWindow(window);
+
+
   return 0;
 }
 
