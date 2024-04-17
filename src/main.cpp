10a122ccdce949b8462bde1278ae167b4c44692e
dormon <imilet@fit.vutbr.cz>
2024-04-17 12:39:27 +0200

    added geGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 
 #include<SDL.h>
+#include<geGL/geGL.h>
+#include<geGL/StaticCalls.h>
+
+using namespace ge::gl;
 
 int main(int argc,char*argv[]){
   std::cerr << "Ahoj" << std::endl;
-  auto window = SDL_CreateWindow("MUL2024",0,0,1024,768,0);
+  auto window = SDL_CreateWindow("MUL2024",0,0,1024,768,SDL_WINDOW_OPENGL);
+
+  auto context = SDL_GL_CreateContext(window);
+
+  ge::gl::init();
 
   bool running = true;
   while(running){// MAIN LOOP
@@ -12,8 +20,14 @@ int main(int argc,char*argv[]){
     while(SDL_PollEvent(&event)){ // EVENT LOOP
       if(event.type == SDL_QUIT)running = false;
     }
+
+    glClearColor(.2,.2,1,1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(window);
   }
 
+
+  SDL_GL_DeleteContext(context);
   SDL_DestroyWindow(window);
 
 
