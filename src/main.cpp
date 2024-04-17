6b11bd6edeb4beb37c5101bf41528cd164a770af
dormon <imilet@fit.vutbr.cz>
2024-04-17 12:32:42 +0200

    window working

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,13 @@ int main(int argc,char*argv[]){
   std::cerr << "Ahoj" << std::endl;
   auto window = SDL_CreateWindow("MUL2024",0,0,1024,768,0);
 
+  bool running = true;
+  while(running){// MAIN LOOP
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){ // EVENT LOOP
+      if(event.type == SDL_QUIT)running = false;
+    }
+  }
 
   SDL_DestroyWindow(window);
 
